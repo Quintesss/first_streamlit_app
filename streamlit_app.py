56dea5717f2f1a9c0a9a6d6b43184ae0a1618c18
Qi Yuan Wong <s10222150@connect.np.edu.sg>
2023-07-03 13:25:44 +0800

    test 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,30 @@
 import streamlit as st
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
+import pandas as pd
 
-st.header('Hello World')
+st.title('Sales Report')
+
+st.header('Q1 Results')
+
+q1_sales = {
+    'January': 100,
+    'February': 110,
+    'March': 115
+}
+
+st.write('January was the start of the year')
+st.write(q1_sales)
+
+st.header('Q2 Results')
+
+q2_sales = {
+    'April': 150,
+    'May': 200,
+    'June': 250
+}
+
+'Q2 had better results:smile:'
+q2_df = pd.DataFrame(q2_sales.items(),
+                     columns=['Month', 'Amount'])
+
+st.table(q2_df)
+st.dataframe(q2_df)
