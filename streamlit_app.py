f7ce0f4105cd366e23f2b93cf2617b698ac58d42
Wong Qi Yuan <116180429+Quintesss@users.noreply.github.com>
2023-07-17 15:11:08 +0800

    added a few inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-
 def main():
     st.title('Customer Prediction Model')
     st.header('Predictions')
@@ -15,15 +14,23 @@ def main():
     st.markdown(html_temp, unsafe_allow_html = True)
     #input elements
 
+    location = st.selectbox(
+        'Select your city. :world_map:',
+        ('Others', 'Boston', 'Denver', 'New York City', 'San Mateo', 'Seattle')
+    )
+
+    avg_qty = st.slider('How many items do you purchase in a typical transaction?', 0, 20, 5)
+    avg_amt = st.number_input('How much do you typically spend in a transaction ($)'
+                              , min_value = 0.00, step = 0.01, value = 25.00, format = "%f")
+
     #input values 
     """
     Location: OHE (might be a challenge, mebbe false everything for now)
     Avg amt & Avg qty
     Age & Gender & Marital & Child count
-    Freq cat & Freq subcat
+    Freq catt & Freq subcat
     """
 
-main()
 #model deployment
 #model = pickle.load(open('cust_analysis_treeClass.pkl','rb'))
 
