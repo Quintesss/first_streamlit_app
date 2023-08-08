d8dda5aef5d8f5fcefecc9deb9270cfa75643089
Wong Qi Yuan <116180429+Quintesss@users.noreply.github.com>
2023-08-08 10:13:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,7 +114,7 @@ def get_counterfactual(predict_x, prediction):
     #different between user and others
     #calculate % change
     percent_chg_amt = (exp_df["AVG_AMT"].mean() - predict_x["AVG_AMT"].iloc[0]) / predict_x["AVG_AMT"].iloc[0] * 100
-    percent_chg_qty = ceiling(exp_df["AVG_QUANTITY"].mean() - predict_x["AVG_QUANTITY"].iloc[0])
+    percent_chg_qty = ceil(exp_df["AVG_QUANTITY"].mean() - predict_x["AVG_QUANTITY"].iloc[0])
 
     st.header('Here\'s a breakdown of your spending habits');
     if (percent_chg_amt >= 0):
@@ -122,7 +122,9 @@ def get_counterfactual(predict_x, prediction):
     elif (percent_chg_amt<0):
         st.subheader("\nYou are currently spending {:.2f}% more than others! :muscle:".format(abs(percent_chg_amt)));
 
-    if (percent_chg_qty >= 0):
+    if (percent_chg_qty == 0):
+        st.subhead("\nYour cart size is just right :pinching_hand:")
+    elif (percent_chg_qty > 0):
         st.subheader("\nYou can put {:d} more items in your basket :shopping_trolley:".format(int(percent_chg_qty)));
     elif (percent_chg_qty<0):
         st.subheader("\nYou have {:d} more items than others! :first_place_medal:".format(abs(int(percent_chg_qty))));
